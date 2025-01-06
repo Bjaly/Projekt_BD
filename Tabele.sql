31bbbc3270cddccbcacef6e11a5ff295c90bc85e
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-07 00:34:11 +0100

    Update Tabele.sql

--- Tabele.sql
+++ Tabele.sql
@@ -17,3 +17,11 @@ CREATE TABLE adresy(
     ulica VARCHAR(50) NOT NULL,
     kod_pocztowy CHAR(6) NOT NULL CHECK (kod_pocztowy REGEXP '^[0-9]{2}-[0-9]{3}$'),
     numer_mieszkania INT NOT NULL);
+-- Tworzenie tabeli opinie
+CREATE TABLE opinie(
+    id VARCHAR(5) PRIMARY KEY,
+    id_klienta VARCHAR(3) NOT NULL,
+    id_produktu CHAR(4) NOT NULL,
+    tresc TEXT,
+    data_opinii DATE NOT NULL,
+    ocena INT NOT NULL);
