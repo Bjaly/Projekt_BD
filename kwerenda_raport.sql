7af8cea90ea669cafa59e5d1029ee840be984041
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-14 20:24:39 +0100

    Update kwerenda_raport.sql

--- kwerenda_raport.sql
+++ kwerenda_raport.sql
@@ -1,17 +1,19 @@
-SELECT  DISTINCT CONCAT(k.imie,' ', k.nazwisko) as "Imie i Nazwisko",
-CASE WHEN k.id = op.id_klienta THEN 'Tak'
-ELSE 'Nie' END AS "Czy opinia?"
-FROM klienci AS k
-LEFT JOIN opinie AS op
-ON k.id = op.id_klienta; 
-
--- CTE srednia klientów
-
 WITH srednia_klientow AS (
-SELECT k.imie,k.nazwisko, AVG(p.kwota) as srednia_klienta
+SELECT k.imie,k.nazwisko,k.id, AVG(p.kwota) as srednia_klienta
 FROM klienci AS k
 INNER JOIN sprzedaze AS s
 ON k.id = s.id_klienta
 INNER JOIN platnosci AS p
 ON s.id_platnosci  = p.id
 GROUP BY k.imie,k.nazwisko)
+SELECT  DISTINCT CONCAT(k.imie,' ', k.nazwisko) as "Imie i Nazwisko", 
+CASE WHEN k.id = op.id_klienta THEN 'Tak'
+	ELSE 'Nie' END AS "Czy opinia?",
+((SELECT AVG(kwota) 
+FROM platnosci) - srednia_klientow.srednia_klienta) AS roznica_miedzy_srednia
+FROM klienci AS k
+LEFT JOIN opinie AS op
+ON k.id = op.id_klienta
+INNER JOIN srednia_klientow
+ON k.id  = srednia_klientow.id
+;
