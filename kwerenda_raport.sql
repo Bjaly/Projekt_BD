e8b596d974a46ecc2cabe57f87ba1cec3fdeddd8
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-14 20:19:30 +0100

    Update kwerenda_raport.sql

--- kwerenda_raport.sql
+++ kwerenda_raport.sql
@@ -5,3 +5,13 @@ FROM klienci AS k
 LEFT JOIN opinie AS op
 ON k.id = op.id_klienta; 
 
+-- CTE srednia klientów
+
+WITH srednia_klientow AS (
+SELECT k.imie,k.nazwisko, AVG(p.kwota) as srednia_klienta
+FROM klienci AS k
+INNER JOIN sprzedaze AS s
+ON k.id = s.id_klienta
+INNER JOIN platnosci AS p
+ON s.id_platnosci  = p.id
+GROUP BY k.imie,k.nazwisko)
