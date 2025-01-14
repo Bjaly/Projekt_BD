9513a0c2f2ed039ba106da4212dbe8946c4a3e25
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-14 22:14:54 +0100

    Update kwerenda_raport.sql

--- kwerenda_raport.sql
+++ kwerenda_raport.sql
@@ -1,19 +1,19 @@
 WITH srednia_klientow AS (
-SELECT k.imie,k.nazwisko,k.id, AVG(p.kwota) as srednia_klienta
+SELECT k.imie,k.nazwisko,k.id, AVG(p.kwota) as srednia
 FROM klienci AS k
 INNER JOIN sprzedaze AS s
 ON k.id = s.id_klienta
 INNER JOIN platnosci AS p
 ON s.id_platnosci  = p.id
 GROUP BY k.imie,k.nazwisko)
-SELECT  DISTINCT CONCAT(k.imie,' ', k.nazwisko) as "Imie i Nazwisko", 
+SELECT DISTINCT CONCAT(k.imie,' ', k.nazwisko) as "Imie i Nazwisko", 
 CASE WHEN k.id = op.id_klienta THEN 'Tak'
 	ELSE 'Nie' END AS "Czy opinia?",
-((SELECT AVG(kwota) 
-FROM platnosci) - srednia_klientow.srednia_klienta) AS roznica_miedzy_srednia
+CONCAT(FORMAT(srednia_klientow.srednia - (SELECT AVG(kwota) FROM platnosci), 2), " zł") AS "Różnica od  średniej"
 FROM klienci AS k
 LEFT JOIN opinie AS op
 ON k.id = op.id_klienta
 INNER JOIN srednia_klientow
 ON k.id  = srednia_klientow.id
+ORDER BY srednia_klientow.srednia - (SELECT AVG(kwota) FROM platnosci) DESC
 ;
