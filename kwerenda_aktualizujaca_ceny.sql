8c97f3957a84c99dcead0d43f1fa52945da3f68e
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-16 16:36:30 +0100

    Update kwerenda_aktualizujaca_ceny.sql

--- kwerenda_aktualizujaca_ceny.sql
+++ kwerenda_aktualizujaca_ceny.sql
@@ -1,8 +1,8 @@
-SELECT
-	nazwa_produktu,
-	(cena_sprzedazy * 0.95) as cena_obnizona ,
-	cena_zakupu,
-	(( cena_sprzedazy * 0.95) - cena_zakupu ) as nowa_marza
+UPDATE
+SET 
+	cena_sprzedazy = cena_sprzedazy * 0.95,
+	marza = (cena_sprzedazy * 0.95) - cena_zakupu
+
 FROM produkty as p 
 WHERE p.kod_produktu IN (
 SELECT kod_produktu 
@@ -12,6 +12,6 @@ FROM (					 -- Subquery zwraca 10 najgorzej sprzedawanych produtków pod względ
 	INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
 	ON p.kod_produktu = ssp.kod_produktu
 	GROUP BY p.kod_produktu
-	ORDER BY SUM(ssp.liczba_pozycji)
+	ORDER BY SUM(ssp.liczba_pozycji) ASC
 	LIMIT 10) AS subquery)
 AND (cena_sprzedazy * 0.95) > (cena_zakupu * 1.09); -- Sprawdzamy aby marża naszego produktu wynosiła conajmniej 9% 
