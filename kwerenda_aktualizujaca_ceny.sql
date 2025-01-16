bca8afa2965cde601994db6224f2a361d9d5cf12
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-16 11:47:54 +0100

    Update kwerenda_aktualizujaca_ceny.sql

--- kwerenda_aktualizujaca_ceny.sql
+++ kwerenda_aktualizujaca_ceny.sql
@@ -1,7 +1,13 @@
-SELECT p.kod_produktu,p.nazwa_produktu, SUM(ssp.liczba_pozycji) as suma_pozycji
-FROM produkty AS p
-INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
-ON p.kod_produktu = ssp.kod_produktu
-GROUP BY p.nazwa_produktu
-ORDER BY suma_pozycji
-LIMIT 5);
+SELECT  nazwa_produktu,(cena_sprzedazy * 0.95) as cena_obnizona , cena_zakupu 
+FROM produkty as p 
+WHERE p.kod_produktu IN (
+SELECT kod_produktu
+FROM (
+	SELECT p.kod_produktu
+	FROM produkty AS p
+	INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
+	ON p.kod_produktu = ssp.kod_produktu
+	GROUP BY p.kod_produktu
+	ORDER BY SUM(ssp.liczba_pozycji)
+	LIMIT 10) AS subquery)
+AND (cena_sprzedazy * 0.95) > (cena_zakupu * 1.07); -- Sprawdzamy aby marża naszego produktu wynosiła conajmniej 7% 
