0eb9f3f79c6b11724e121c7f5246d0c7828d71dd
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-16 16:49:43 +0100

    Update kwerenda_aktualizujaca_ceny.sql

--- kwerenda_aktualizujaca_ceny.sql
+++ kwerenda_aktualizujaca_ceny.sql
@@ -1,16 +1,17 @@
--- Kwerenda obniża ceny najgorzej sprzedających się produktów o 5%
--- Tworzymy CTE ktore znajduje 10 najgorzej sprzedajacych sie produktow wedlug wolumenu sprzedazy
-WITH najgorzej_sprzedajace AS (
-    SELECT p.kod_produktu
-    FROM produkty AS p
-    INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
-    ON p.kod_produktu = ssp.kod_produktu
-    GROUP BY p.kod_produktu
-    ORDER BY SUM(ssp.liczba_pozycji) ASC
-    LIMIT 10
-)
+-- Kwerenda aktualizuje cenę sprzedaży najgorzej sprzedających się produktów w oparciu o wulumen
 UPDATE produkty
 SET cena_sprzedazy = cena_sprzedazy * 0.95,
     marza = (cena_sprzedazy * 0.95) - cena_zakupu
-WHERE kod_produktu IN (SELECT kod_produktu FROM NajgorzejSprzedajace)
-AND (cena_sprzedazy * 0.95) >= (cena_zakupu * 1.09); -- Sprawdzamy aby produkt po obnizonej cenie mial marze conajmniej 9% (nie chcemy sprzedawać produktów na których nie zarobimy
+WHERE kod_produktu IN (   
+    SELECT kod_produktu
+    FROM (
+        SELECT p.kod_produktu
+        FROM produkty AS p
+        INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
+        ON p.kod_produktu = ssp.kod_produktu
+        GROUP BY p.kod_produktu
+        ORDER BY SUM(ssp.liczba_pozycji) ASC
+        LIMIT 10 -- Bierzemy 10 najgorzej sprzedająych się produktów
+    ) AS subquery 
+)
+AND (cena_sprzedazy * 0.95) >= (cena_zakupu * 1.09); -- Określamy warunki że obniżka cen spowoduje obniżenie marży nie mniej niż do 9%
