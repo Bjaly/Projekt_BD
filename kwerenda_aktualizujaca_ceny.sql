2d00cee2b53a8fa315f844e37bf65c6ada467aa7
Dawid <76225471+Bjaly@users.noreply.github.com>
2025-01-16 11:12:46 +0100

    Update kwerenda_aktualizujaca_ceny.sql

--- kwerenda_aktualizujaca_ceny.sql
+++ kwerenda_aktualizujaca_ceny.sql
@@ -1,1 +1,7 @@
-
+SELECT p.kod_produktu,p.nazwa_produktu, SUM(ssp.liczba_pozycji) as suma_pozycji
+FROM produkty AS p
+INNER JOIN szczegoly_sprzedanych_pozycji AS ssp
+ON p.kod_produktu = ssp.kod_produktu
+GROUP BY p.nazwa_produktu
+ORDER BY suma_pozycji
+LIMIT 5);
